360d2efa72c09bca8c5df111280eb886cc95307a
Muhammed Cayhan <mcayhan2004@gmail.com>
2026-07-20 13:26:05 +0300

    feat: create generic repository

---
+++ app/src/main/java/com/example/myapplication/Repository.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import java.util.ArrayList;
+
+public class Repository <T>{
+    private ArrayList<T> items = new ArrayList<>();
+
+    public void add(T item){
+        items.add(item);
+        System.out.println("Item added.");
+    }
+
+    public T get(int index) throws RecordNotFoundException{
+        if (index<0 || index >= items.size()){
+            throw new RecordNotFoundException("Item not found at index: " + index);
+        }
+        return items.get(index);
+    }
+
+    public void delete(int index) throws RecordNotFoundException{
+        if(index<0 || index>= items.size()){
+            throw new RecordNotFoundException("Cannot delete. Item not found at index." + index);
+        }
+        items.remove(index);
+        System.out.println("Item deleted.");
+    }
+
+}
