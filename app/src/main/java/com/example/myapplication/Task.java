97e69cadf8c0c18086f703d822d5c5ac03e8161d
Muhammed Cayhan <mcayhan2004@gmail.com>
2026-07-27 16:49:23 +0300

    feat(task-manager): create Task model with encapsulation

--- app/src/main/java/com/example/myapplication/Task.java
+++ app/src/main/java/com/example/myapplication/Task.java
@@ -1,4 +1,48 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+
 public class Task {
+    private final int id;
+    private String title;
+    private boolean isCompleted;
+
+    public Task(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        String status;
+
+        if (isCompleted) {
+            status = "Completed";
+        } else {
+            status = "Pending";
+        }
+
+        return "Task ID: " + id + " | Title: '" + title + "' | Status: " + status;
+    }
 }
