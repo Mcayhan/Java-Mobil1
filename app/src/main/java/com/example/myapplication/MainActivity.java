4435faee429fad45ff0fa82f5af48682c7fae121
Muhammed Cayhan <mcayhan2004@gmail.com>
2026-07-27 16:51:01 +0300

    feat(task-manager): integrate task operations into MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +17,28 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         System.out.println("Hello World");
+
+
+        TaskManager taskManager = new TaskManager();
+
+        // add task
+        taskManager.addTask(new Task(1, "Setup Android Studio Environment"));
+        taskManager.addTask(new Task(2, "Complete Day 8 Task Manager Consolidation"));
+
+        // listing
+        taskManager.logAllTasks();
+
+        // complete task
+        taskManager.completeTask(1);
+
+        // exception test
+        taskManager.completeTask(99);
+
+        // currents status
+        taskManager.logAllTasks();
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
