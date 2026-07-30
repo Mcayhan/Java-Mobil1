057eb0d263908d68e4c0bf948b4940b228d29b5d
Muhammed Cayhan <mcayhan2004@gmail.com>
2026-07-30 23:39:13 +0300

    feat(room): setup DAO and database configuration

--- app/src/main/java/com/example/myapplication/NoteDao.java
+++ app/src/main/java/com/example/myapplication/NoteDao.java
@@ -1,4 +1,29 @@
 package com.example.myapplication;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
 public interface NoteDao {
+
+    //adding new note method. Room will fill here.
+    @Insert
+    void insertNote(Note note);
+
+    //listing code. sqll code for bring all table
+    @Query("SELECT * FROM notes")
+    List<Note> getAllNotes();
+
+    //update note
+    @Update
+    void updateNote(Note note);
+
+    //delete note
+    @Delete
+    void deleteNote(Note note);
 }
