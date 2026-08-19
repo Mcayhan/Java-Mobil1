3c5758f712f47e80040df5936a13d5435898d592
Muhammed Cayhan <mcayhan2004@gmail.com>
2026-08-19 17:55:48 +0300

    feat: trigger secure storage demo on app launch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,4 +41,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
+    implementation("androidx.security:security-crypto:1.1.0-alpha06")
 }
